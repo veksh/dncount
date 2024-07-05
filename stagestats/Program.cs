ea5c2362f37ee44731843acd0103ecda730646c4
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-07-05 22:10:57 +0000

    output resulting counts

--- stagestats/Program.cs
+++ stagestats/Program.cs
@@ -2,7 +2,8 @@
 
 using Stage;
 
-var scount = new StageCounter(["raz", "dva"]);
+var stageNames = new string[]{"raz", "dva"};
+var scount = new StageCounter(stageNames);
 var res1 = scount.AddParticipant(new Dictionary<string, string>{["raz"] = "-", ["dva"] = "-"});
 Console.WriteLine($"all ok for first: {res1}");
 var res2 = scount.AddParticipant(new Dictionary<string, string>{["raz"] = "17:15", ["dva"] = "-"});
@@ -11,3 +12,8 @@ var res3 = scount.AddParticipant(new Dictionary<string, string>{["raz"] = "07:15
 Console.WriteLine($"all ok for third: {res3}");
 var res4 = scount.AddParticipant(new Dictionary<string, string>{["raz"] = "-", ["dva"] = "18:00"});
 Console.WriteLine($"here comes the invalid: {res4}");
+
+Console.WriteLine(string.Join(", ", stageNames.Select(sn => $"{sn}: {scount.GetCount(sn)}")));
+Console.WriteLine(string.Join(", ", Enum.GetValues(typeof(ParticipantStatus))
+  .Cast<ParticipantStatus>()
+  .Select(sn => $"{sn}: {scount.GetStatusCount(sn)}")));
