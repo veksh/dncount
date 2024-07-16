a234bde6c33fa1180d62e6fc9626f28212a0b7ef
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-07-16 18:41:37 +0000

    lots of web service bells and whistles

--- stageweb/Program.cs
+++ stageweb/Program.cs
@@ -10,7 +10,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy (
+        policy => {
+    });
+});
+builder.Services.ConfigureHttpJsonOptions(options => {
+    options.SerializerOptions.WriteIndented = true;
+    // options.SerializerOptions.IncludeFields = true;
+});
 
+// cons log: https://learn.microsoft.com/en-us/dotnet/core/extensions/console-log-formatter
+// builder.Logging.AddJsonConsole();
+builder.Logging.AddSimpleConsole(options => {
+    options.IncludeScopes = true;
+    options.SingleLine = true;
+    options.TimestampFormat = "HH:mm:ss "; // "yyyy-MM-dd HH:mm:ss zzz"
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,26 +34,41 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    // conf: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-8.0
+    // var message = app.Configuration["MyKey"] ?? "undef";
+    // var title = Configuration["Position:Title"];
+    // var number = Configuration.GetValue<int>("NumberKey", 99)
+    // env: builder.Configuration.AddEnvironmentVariables(prefix: "MYAPPENV_");
 }
+// app.UseHttpsRedirection();
+app.UseCors();
 
-app.UseHttpsRedirection();
+// see https://learn.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-8.0
+app.Logger.LogInformation("The app started");
 
+// optional: string? gender, ... gender ?? "yes"
 app.MapGet("/chartdata", (int courseNo = 100) =>
 {
     var stageNames = new string[]{"start", "middle", "finish"};
     var stages = stageNames.Select(stageName =>
-        new Stageweb.RaceStage(stageName, Random.Shared.Next(0, 100))
+        new Stageweb.RaceStage(stageName, Random.Shared.Next(0, courseNo))
     ).ToList();
     var stateNames = new string[]{"not_started", "running", "done"};
     var states = stateNames.Select(stateName =>
-        new Stageweb.StatusInfo(stateName, Random.Shared.Next(0, 100))
+        new Stageweb.StatusInfo(stateName, Random.Shared.Next(0, courseNo))
     ).ToList();
     var res = new Stageweb.RaceInfo(stages, states);
-    return res;
+    app.Logger.LogInformation("processeed");
+    // or just `return res`;
+    // alt: Results.NotFound()|UnprocessableEntity()|Content(data, "application/json");
+    return Results.Ok(res);
 })
 .WithName("chartdata")
 .WithOpenApi();
 
+// var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+// app.Run($"http://localhost:{port}");
 app.Run();
 
 // fine-tune appearance: [JsonPropertyName("text")] [JsonInclude]
